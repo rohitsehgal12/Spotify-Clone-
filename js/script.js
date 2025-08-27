db7c0f652035a01661f6d2d3e43b31ac71cce90b
rohitsehgal12 <20rohitsehgal@gmail.com>
2025-08-27 10:16:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ let currfolder;
 
 async function getsongs(folder) {
     currfolder = folder;
-    let a = await fetch(`/${folder}/`);
+    let a = await fetch(`/songs/ncs/list.json`);
     let response = await a.text();
     console.log(response)
     let div = document.createElement("div")
